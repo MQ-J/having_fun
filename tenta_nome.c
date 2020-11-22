1a0a9b892c3cd6cfdb523a7ff19e0358e6e6de2b
Marcos Querino <61765516+MQ-J@users.noreply.github.com>
2020-11-22 19:02:38 -0300

    Create tenta_nome.c
    programa em modelo interativo: leia o código para aprender a usar
    ;)

---
+++ tenta_nome.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+
+//troque as letras deste vetor pelas letras do seu nome (aceita espaço):
+char vet[] = {'a', 'q', 'u', 'i', ' ', 'o', 'l', 'h', 'a'};
+
+int main()
+{
+  int i, a;
+  srand(time(NULL));
+  /*mude o número "8" abaixo pelo número de caracteres que você colocou no vetor, menos um*/
+  for (i=0; i <= 8; i++)
+  {
+    /*mude o número "9" abaixo pelo número de caracteres que você colocou no vetor*/
+    a = (rand() % 9);
+    printf("%c", vet[a]);
+  }
+  return 0;
+}
